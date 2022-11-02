c6d33b1991578ad18e79dabe683e9d2665571283
Tyler Reddy <tyler.je.reddy@gmail.com>
2022-11-02 17:47:48 -0600

    ENH: round, trunc, ceil, floor now passing API tests..

--- generate_ufuncs.py
+++ generate_ufuncs.py
@@ -4,7 +4,7 @@ def main(ufunc_mod_path: str):
     # automatically write out some ufunc workunits + exposed
     # functions for pykokkos, to reduce the copy-paste burden
     with open(ufunc_mod_path, 'w') as ufunc_module:
-        for ufunc_name in ["trunc"]:
+        for ufunc_name in ["round", "trunc", "ceil", "floor"]:
             for view_type in ["double",
                               "float",
                               "int8",
@@ -15,11 +15,15 @@ def main(ufunc_mod_path: str):
                               "uint16",
                               "uint32",
                               "uint64"]:
+                if "int" in view_type:
+                    # special cased
+                    continue
                 ufunc_module.write(template.template_str_workunit.format(ufunc_name=ufunc_name,
                                                                      view_type=view_type))
             ufunc_module.write(template.template_str_main_func_top.format(ufunc_name=ufunc_name,
                                                                  view_type=view_type))
             for view_type in ["double",
+                              "float64",
                               "float",
                               "uint8",
                               "uint16",
@@ -29,12 +33,20 @@ def main(ufunc_mod_path: str):
                               "int16",
                               "int32",
                               "int64"]:
+                if "int" in view_type:
+                    # special cased
+                    continue
                 if view_type == "double":
                     cond = "if"
                 else:
                     cond = "elif"
+                if view_type == "float64":
+                    view_type_2 = "double"
+                else:
+                    view_type_2 = view_type
                 ufunc_module.write(template.template_str_main_func_mid.format(ufunc_name=ufunc_name,
                                                                      view_type=view_type,
+                                                                     view_type_2=view_type_2,
                                                                      cond=cond))
 
             ufunc_module.write(template.template_str_main_func_bottom.format(ufunc_name=ufunc_name,
--- template.py
+++ template.py
@@ -20,19 +20,24 @@ def {ufunc_name}_impl_3d_{view_type}(tid: int, view: pk.View3D[pk.{view_type}], 
 
 template_str_main_func_top = '''
 def {ufunc_name}(view):
+    if "int" in str(view.dtype):
+        # special case defined in API std
+        return view
+    out = pk.View(view.shape, dtype=view.dtype)
     if len(view.shape) > 3:
         raise NotImplementedError("only up to 3D views currently supported for {ufunc_name}() ufunc.")
         '''
 
 template_str_main_func_mid = '''
     {cond} "{view_type}" in str(view.dtype):
-        out = pk.View([*view.shape], dtype=pk.{view_type})
-        if len(view.shape) == 1:
-            pk.parallel_for(view.shape[0], {ufunc_name}_impl_1d_{view_type}, view=view, out=out)
+        if view.shape == ():
+            pk.parallel_for(1, {ufunc_name}_impl_1d_{view_type_2}, view=view, out=out)
+        elif len(view.shape) == 1:
+            pk.parallel_for(view.shape[0], {ufunc_name}_impl_1d_{view_type_2}, view=view, out=out)
         elif len(view.shape) == 2:
-            pk.parallel_for(view.shape[0], {ufunc_name}_impl_2d_{view_type}, view=view, out=out)
+            pk.parallel_for(view.shape[0], {ufunc_name}_impl_2d_{view_type_2}, view=view, out=out)
         elif len(view.shape) == 3:
-            pk.parallel_for(view.shape[0], {ufunc_name}_impl_3d_{view_type}, view=view, out=out)
+            pk.parallel_for(view.shape[0], {ufunc_name}_impl_3d_{view_type_2}, view=view, out=out)
 '''
 
 template_str_main_func_bottom = '''
